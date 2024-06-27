26167204df4dae62dc6406d948b886b36b760a4b
naufal <askpradana@gmail.com>
2024-06-27 14:29:23 +0700

    init commit

---
+++ src/components/CustomNavbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Triangle } from 'lucide-vue-next'
+</script>
+
+<template>
+  <nav class="flex flex-row gap-4">
+    <div>
+      <Triangle />
+      <p>AirBNB</p>
+    </div>
+
+    <div></div>
+  </nav>
+</template>
